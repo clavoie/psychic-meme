abd4c921c5afa8692af44fccd666bb8528621df0
Chris LaVoie <clavoie@gmail.com>
2014-11-15 21:55:05 -0500

    adding unit tests around the public api

--- src/psychic_meme/dawg.clj
+++ src/psychic_meme/dawg.clj
@@ -2,12 +2,16 @@
   (:require [psychic-meme.private.dawg :as private]))
 
 (defn create
-  "Creates a new directed acyclic word graph"
+  "Creates a new directed acyclic graph"
   []
   (sorted-map nil (private/create-node nil)))
 
 (defn add
-  "Adds the items of a seq to the graph"
+  "Adds the items of a seq to the graph
+
+  dawg - the graph to add the items to
+  value - the value to add to the graph. (seq value) will be used to create the
+          sequence which is added to the graph"
   [dawg value]
   (let [seq-value (seq value)]
     (loop [node-id nil
@@ -21,3 +25,9 @@
                  (first other-values)
                  (rest other-values)
                  dawg))))))
+
+(defn items
+  "Returns a lazy sequence of all the items in the graph"
+  [graph node-id]
+  (if (= :eos node-id) nil
+    (map #(cons (private/get-value graph %) (lazy-seq (items graph %))) (private/get-edges graph node-id))))
---
+++ test/psychic_meme/dawg_test.clj
@@ -0,0 +1,25 @@
+(ns psychic-meme.dawg-test
+  (:require [clojure.test :refer :all]
+            [psychic-meme.dawg :refer :all]
+            [psychic-meme.private.dawg :as private]))
+
+(deftest create-test
+  (testing "Creating graph"
+    (let [graph (create)]
+      (is (map? graph))
+      (is (sorted? graph))
+      (is (not (nil? (get graph nil)))))))
+
+(deftest add-test
+  (testing "Adding an item to the graph"
+    (let [graph (create)
+          graph (add (add graph "hello") "hi")
+          node-count (count graph)
+          root-edges (count (private/get-edges  graph nil))]
+      (is (= node-count 7))
+      (is (= root-edges 2)))))
+
+(deftest items-test
+  (let [graph (create)
+        graph (add graph "hello")]
+    (println (items graph nil))))
