26bd008a5cea177b19df8b572ad2274209f4ecb2
Chris LaVoie <clavoie@gmail.com>
2014-12-03 11:14:59 -0500

    reverting to v0.6.6 of codox until the windows path normalization bug is fixed

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :global-vars {*warn-on-reflection* true}
-  :plugins [[codox "0.8.10"]]
+  :plugins [[codox "0.6.6"]]
   :codox {:src-dir-uri "http://github.com/clavoie/psychic-meme/blob/master/"
           :src-linenum-anchor-prefix "L"})
