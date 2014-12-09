62676dd9573be900e566bad6d0b20d4e56d7b74e
Chris LaVoie <clavoie@gmail.com>
2014-12-09 18:34:34 -0500

    bumping rev number to v1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject psychic-meme "1.0.0-SNAPSHOT"
+(defproject psychic-meme "1.0.0"
   :description "Radix tree for Clojure"
   :url "https://github.com/clavoie/psychic-meme"
   :license {:name "Eclipse Public License"
