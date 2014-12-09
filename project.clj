160e41cf19234a6bfee5b51636d90af93dedf90a
Chris LaVoie <clavoie@gmail.com>
2014-12-09 18:30:32 -0500

    fixed the project's description to match what the project eventually ended up being

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject psychic-meme "0.1.0-SNAPSHOT"
-  :description "Directed acyclic word graph for Clojure"
+  :description "Radix tree for Clojure"
   :url "https://github.com/clavoie/psychic-meme"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
